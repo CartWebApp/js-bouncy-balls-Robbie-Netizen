cfbefc2a441c1d3bbc49427561a4824f6c8c2507
Robbie-Netizen <robbie@tringali.me>
2026-04-22 14:48:04 -0700

    Made bouncy balls

--- main.js
+++ main.js
@@ -11,4 +11,87 @@ const height = canvas.height = window.innerHeight;
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
-}
+};
+
+function Ball(x, y, velx, vely, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velx = velx;
+  this.vely = vely;
+  this.color = color;
+  this.size = size;
+};
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velx = -(this.velx);
+  };
+  if ((this.x - this.size) <= 0) { // this was 'width' instead of 0
+    this.velx = -(this.velx);
+  };
+  if ((this.y + this.size) >= height) {
+    this.vely = -(this.vely);
+  };
+  if ((this.y - this.size) <= 0) { // this was 'height' instead of 0
+    this.vely = -(this.vely);
+  };
+
+  // the logic above detects when a ball is at the edge of the canvas and 'inverts' it's direction of travel when it hits an edge, having both ends of the canvas a.k.a values for each width and height look at the "width" or "height" binding did not give the balls room to "move" about the canvas. Essentially the logic was that the opposing edges of the canvas were in the same location and had zero units of space between them so your balls were frozen in place.
+
+  this.x += this.velx;
+  this.y += this.vely;
+};
+
+Ball.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+        balls[j].velx = -(balls[j].velx);
+        balls[j].vely = -(balls[j].vely);
+      };
+    };
+  };
+};
+
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10, 20);
+  let ball = new Ball( 
+    //ball position always drawn at least one ball width away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+    size
+  );
+
+  balls.push(ball);
+};
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, .25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  };
+
+  requestAnimationFrame(loop);
+};
+loop();
